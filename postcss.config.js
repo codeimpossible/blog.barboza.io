ace6dc2ba0567eec3c69e99a2500d9e036a0819b
Jared Barboza <jared.m.barboza@gmail.com>
2025-04-24 06:57:29 -0400

    upgrade dependencies, fix some formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss')('./tailwind.config.js'),
-    require('postcss-nested'),
-    require('autoprefixer'),
-  ],
+    plugins: [
+        require('postcss-import'),
+        require('@tailwindcss/postcss'),
+        require('postcss-nested'),
+        require('autoprefixer'),
+    ],
 };
