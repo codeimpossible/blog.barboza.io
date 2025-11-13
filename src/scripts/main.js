ddbf3e6e267f5dfb7143c06e87da7eab60e0c5a8
Jared Barboza <jared.m.barboza@gmail.com>
2025-11-13 11:48:42 -0500

    theme adjustments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,49 @@
-import '../styles/tailwind.css';
-import { handleCodeCopying } from './copy.js';
+import "../styles/tailwind.css";
+import { handleCodeCopying } from "./copy.js";
 
-if (DEV_MODE) console.log('Dev mode is currently enabled.');
+if (DEV_MODE) console.log("Dev mode is currently enabled.");
 
+const updateTheme = () => {
+  const isDark =
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches);
+  document.documentElement.setAttribute("data-theme", isDark ? "dark" : null);
+};
+
+const lightMode = () => {
+  localStorage.theme = "light";
+};
+
+const darkMode = () => {
+  localStorage.theme = "dark";
+};
+
+const resetTheme = () => {
+  localStorage.removeItem("theme");
+};
+
+const toggleTheme = () => {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  console.log(prefersDark, localStorage.theme);
+  if (!localStorage.theme) {
+    (prefersDark ? lightMode : darkMode)();
+    updateTheme();
+    return;
+  }
+  if ("theme" in localStorage && localStorage.theme === "dark") {
+    lightMode();
+  } else {
+    darkMode();
+  }
+  updateTheme();
+};
+
+updateTheme();
 handleCodeCopying();
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("theme-toggle");
+  toggle.addEventListener("click", () => {
+    toggleTheme();
+  });
+});
