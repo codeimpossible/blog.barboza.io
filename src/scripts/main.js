2dd619d3076dbb83ca970447d8b7d1cf3e0ce9e1
Jared Barboza <jared.m.barboza@gmail.com>
2025-11-16 09:18:59 -0500

    tweaking colors and the tags index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,37 @@
-import "../styles/tailwind.css";
-import { handleCodeCopying } from "./copy.js";
+import '../styles/tailwind.css';
+import { handleCodeCopying } from './copy.js';
 
-if (DEV_MODE) console.log("Dev mode is currently enabled.");
+if (DEV_MODE) console.log('Dev mode is currently enabled.');
 
 const updateTheme = () => {
   const isDark =
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches);
-  document.documentElement.setAttribute("data-theme", isDark ? "dark" : null);
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches);
+  document.documentElement.setAttribute('data-theme', isDark ? 'dark' : 'light');
 };
 
 const lightMode = () => {
-  localStorage.theme = "light";
+  localStorage.theme = 'light';
 };
 
 const darkMode = () => {
-  localStorage.theme = "dark";
+  localStorage.theme = 'dark';
 };
 
 const resetTheme = () => {
-  localStorage.removeItem("theme");
+  localStorage.removeItem('theme');
 };
 
 const toggleTheme = () => {
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   console.log(prefersDark, localStorage.theme);
   if (!localStorage.theme) {
     (prefersDark ? lightMode : darkMode)();
     updateTheme();
     return;
   }
-  if ("theme" in localStorage && localStorage.theme === "dark") {
+  if ('theme' in localStorage && localStorage.theme === 'dark') {
     lightMode();
   } else {
     darkMode();
@@ -41,9 +41,9 @@ const toggleTheme = () => {
 
 updateTheme();
 handleCodeCopying();
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("theme-toggle");
-  toggle.addEventListener("click", () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('theme-toggle');
+  toggle.addEventListener('click', () => {
     toggleTheme();
   });
 });
